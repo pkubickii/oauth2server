0be1a30a366bd18aa70283d09df160a30442dc0b
pkubickii <kubicki.przemyslaw@gmail.com>
2023-02-18 14:13:30 +0100

    user login + redirect/?code=usercode

--- app.js
+++ app.js
@@ -11,13 +11,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", "./views");
 app.set("view engine", "pug");
+const redirect_uri = `http://localhost:3000/?code=`;
 
 app.get("/", (req, res) => {
   res.send("Welcome to oAuth2 authorization server");
 });
+
 app.get("/register", (req, res) => {
   res.render("register");
 });
+
 app.post("/register", (req, res) => {
   const { username, password } = req.body;
   if (!req.body || username.length === 0 || password.length === 0) {
@@ -31,13 +34,43 @@ app.post("/register", (req, res) => {
     console.log(`password: ${password}`);
     console.log(`Client ID: ${clientId}`);
     console.log(`Client Secret: ${clientSecret}`);
+
     const query = `INSERT INTO client (username, password, client_id, client_secret) VALUES ('${username}', '${password}','${clientId}', '${clientSecret}')`;
     executeQuery(query);
+
     res
       .status(200)
       .send(`client id: ${clientId} <br> client secret: ${clientSecret}`);
   }
 });
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!req.body || username.length === 0 || password.length === 0) {
+    res.status(400).send(`Invalid input!`);
+  } else {
+    const query = `SELECT code FROM user WHERE username = '${username}'`;
+    executeQuery(query)
+      .then((result) => {
+        if (result.length === 0) {
+          res.status(404).send("User not found");
+        } else {
+          const code = result[0].code;
+          console.log(`query result: ${JSON.stringify(result)}`);
+          res.redirect(`${redirect_uri}${code}`);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send("Error fetching user data");
+      });
+  }
+});
+
 app.listen(port, () => {
   console.log(`oAuth2 Server is running on port ${port}`);
 });
--- db.js
+++ db.js
@@ -1,29 +1,22 @@
 const mysql = require("mysql2");
 
-const executeQuery = (query) => {
-  const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "oa2",
-    password: "oa2password",
-    database: "authdb",
-  });
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "oa2",
+  password: "oa2password",
+  database: "authdb",
+});
 
-  connection.connect((err) => {
-    if (err) {
-      console.error("Error connecting to MySQL database: " + err.stack);
-      return;
-    }
-    console.log("Connected to MySQL database as id " + connection.threadId);
-  });
-
-  connection.query(query, (error, results, fields) => {
-    if (error) {
-      console.error("Error executing query: " + error.stack);
-      return;
-    }
+async function executeQuery(query) {
+  try {
+    const [rows, fields] = await connection.promise().query(query);
+    return rows;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  } finally {
     connection.end();
-    return results;
-  });
-};
+  }
+}
 
 module.exports = { executeQuery };
